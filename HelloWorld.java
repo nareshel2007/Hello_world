739fcc8c87712e05965ac2710d651443b173f1af
naresh <nareshel2007@gmail.com>
2022-03-17 08:13:09 +0000

    Hello world java changed to webhook Done changes from ec2 instance remote

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello,Tomorrow is Holi .Happy Holi");
     System.out.println("Hello,This is webhook - Git hub hook web");
-
+    System.out.println("Hello,This is webhook - Git hub From Ec2 instace remote");
     
   }
 }
