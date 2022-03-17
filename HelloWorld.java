7e5ec91e2cfa0efee21826482080800df6945f45
naresh <nareshel2007@gmail.com>
2022-03-17 05:23:29 +0000

    Hello world java changed to Happy second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello,Tomorrow is Holi .Happy Holi");
     System.out.println("Hello,Happy long weekend");
+    System.out.println("Hello,Happy This is the sencond commit");
   }
 i}
