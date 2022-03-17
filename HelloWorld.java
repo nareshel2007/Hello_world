147f696bc823534da68a4d1591e93d996c78cb91
naresh <nareshel2007@gmail.com>
2022-03-17 05:18:07 +0000

    Hello world java changed to Happy long weekend

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello,Tomorrow is Holi .Happy Holi");
+    System.out.println("Hello,Happy long weekend");
   }
-}
+i}
